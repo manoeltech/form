e1141e106a5e005d0e198ff11b5aac28ca3c7a00
manoeltech <manoel.programador21@gmail.com>
2023-07-05 10:14:31 -0300

    Improve form.js

--- js/form.js
+++ js/form.js
@@ -5,4 +5,7 @@ const textResult = document.querySelector('.text-result')
 btnSubmit.addEventListener('click', function(){
   let email = getEmail.value
   textResult.innerHTML = `O email ${email} foi cadastrado com sucesso!`
+  textResult.innerHTML = `O email ${email} foi cadastrado com sucesso! <br>
+                                           <i class="fa-solid fa-file-pen" style="color: #00ffcc;">
+                                           </i> <a href='edit.html'>Editar email</a>`;
 })
